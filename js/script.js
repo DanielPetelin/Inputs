58244053d145d66b6ad25a352f65da034e09a51c
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-12 15:14:16 +0300

    add two fun

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ document.querySelector('.one').onclick = () => {
     let a = document.querySelector('.i-1').value;
     console.log(a);
 };
+
+document.querySelector('.two').onclick = () => {
+    let b = +document.querySelector('.i-2').value;
+    console.log(b + 5);
+};
+
+document.querySelector('.color').onclick = () => {
+    let c = document.querySelector('.i-3').value;
+    document.querySelector('.color').style.background = c;
+    console.log(c);
+};
